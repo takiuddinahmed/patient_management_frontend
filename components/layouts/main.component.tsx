9b7607d57932d2463daab2519e4b3f3be2dab893
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2022-08-04 14:21:30 +0600

    add main props

--- components/layouts/main.component.tsx
+++ components/layouts/main.component.tsx
@@ -1,11 +1,17 @@
 import Navbar from "./navbar.component";
+improt {FC} from "react";
 
-const Main = (props: any) => {
+interface IProps {
+  loading?: boolean;
+  children?: any;
+}
+
+const Main: FC<IProps> = ({loading, children}) => {
   return (
     <>
       <Navbar />
       <div className="m-6 h-full flex justify-center items-center flex-col mx-auto lg:w-1/2 ">
-        {props.children}
+        {children}
       </div>
     </>
   );
