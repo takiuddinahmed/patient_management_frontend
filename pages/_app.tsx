8863b15fd86f68a7b334b05b433be615d54ec497
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2022-10-29 01:24:32 +0600

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,46 +8,46 @@ import { signOut } from "firebase/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  useEffect(() => {
-    const checkUser = async () => {
-      const user = auth.currentUser;
-      const userData = await getDocs(
-        query(collection(db, "users"), where("uid", "==", user?.uid))
-      );
+  // useEffect(() => {
+  //   const checkUser = async () => {
+  //     const user = auth.currentUser;
+  //     const userData = await getDocs(
+  //       query(collection(db, "users"), where("uid", "==", user?.uid))
+  //     );
 
-      userData.docs.forEach((doc) => {
-        console.log(doc.data());
-        const data = doc.data();
-        if (
-          data?.userRole == "doctor" &&
-          !router.pathname.startsWith("doctor")
-        ) {
-          router.push("/doctor");
-        } else if (
-          data?.userRole == "pharmacy" &&
-          !router.pathname.startsWith("pharmacy")
-        ) {
-          router.push("/pharmacy");
-        } else if (
-          data?.userRole == "lab" &&
-          !router.pathname.startsWith("laboratory")
-        ) {
-          router.push("/laboratory");
-        } else if (
-          data?.userRole == "patient" &&
-          !router.pathname.startsWith("patient")
-        ) {
-          router.push("/patient/" + data.uid);
-        }
-      });
-    };
-    console.log(auth.currentUser);
-    if (auth.currentUser) {
-      checkUser();
-    } else {
-      router.push("/auth/login");
-    }
-  }, []);
+  //     userData.docs.forEach((doc) => {
+  //       console.log(doc.data());
+  //       const data = doc.data();
+  //       if (
+  //         data?.userRole == "doctor" &&
+  //         !router.pathname.startsWith("doctor")
+  //       ) {
+  //         router.push("/doctor");
+  //       } else if (
+  //         data?.userRole == "pharmacy" &&
+  //         !router.pathname.startsWith("pharmacy")
+  //       ) {
+  //         router.push("/pharmacy");
+  //       } else if (
+  //         data?.userRole == "lab" &&
+  //         !router.pathname.startsWith("laboratory")
+  //       ) {
+  //         router.push("/laboratory");
+  //       } else if (
+  //         data?.userRole == "patient" &&
+  //         !router.pathname.startsWith("patient")
+  //       ) {
+  //         router.push("/patient/" + data.uid);
+  //       }
+  //     });
+  //   };
+  //   console.log(auth.currentUser);
+  //   if (auth.currentUser) {
+  //     checkUser();
+  //   } else {
+  //     router.push("/auth/login");
+  //   }
+  // }, []);
 
   return <Component {...pageProps} />;
 }
